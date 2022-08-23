5c82eb5d16d5d5559a0e693af9efd228243163c7
weinbergerlab <dweinber@gmail.com>
2022-08-23 17:16:25 -0400

    test alternative structures and incorporate count data from original trial

--- R/jags_basic_pois.R
+++ R/jags_basic_pois.R
@@ -1,18 +1,27 @@
 model_string_basic_pois <- "
 model{
 for(i in 1:2){ 
+  N_cases_orig[i] ~ dpois(mu[i])
+
   N_cases[i] ~ dpois(lambda[i])
   
-  log(lambda[i]) <- int  +  vax[i]*beta1 + log(pop[i])
+  log(lambda[i]) <- int  +  vax[i]*beta1 + log(pop[i]*100000)
+  
+  log(mu[i]) <- int_orig + (i-1)*delta + log(pop_orig[i]*100000) #this is just included as a check--do we get same estimates out as provided in original study when we start with the counts?
 
 } 
   
+
 
   int ~ dnorm(0, 1e-4)
   
   beta1 ~ dnorm(log_irr.obs, prec.log.irr.obs )
 
-  actual_prior_prec =prec.log.irr.obs
+
+  int_orig ~ dnorm(0, 1e-4)
+
+  delta ~ dnorm(0, 1e-4)
+
   alpha=1
 }
 "
