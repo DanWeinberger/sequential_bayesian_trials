a8afcded75c3fadd9b607cd4413667049beb52a4
Weinberger <daniel.weinberger@yale.edu>
2022-09-06 17:19:58 -0400

    annotate

--- R/jags_modified_pois_commensurate_gamma.R
+++ R/jags_modified_pois_commensurate_gamma.R
@@ -11,15 +11,17 @@ for(i in 1:2){
 } 
   
 #Priors for original trial
- # int_orig ~ dnorm(0, 0.1)
   int_orig ~ dnorm(0, 1e-4)
   delta ~ dnorm(0, 1e-4) #uninformative prior for original trial
+
 # Priors from new trial
   int ~ dnorm(int_orig, tau2)
-  beta1 ~ dnorm(delta, tau) #beta centered on alpha with highlight informative prior, which can become less informative if it does not match
- # tau ~ dgamma(1, 0.001) # gamma hyperprior from psborrow
+  beta1 ~ dnorm(delta, tau) #beta centered on delta with highly informative prior, which can become less informative if it does not match
+
   tau ~ dgamma(0.01, 0.01) # gamma hyperprior from psborrow
   tau2 ~ dgamma(0.01, 0.01) # gamma hyperprior from psborrow
+	    
+	    #these don't do anything; they are just carried through so function works
 	    a1 <- prec.log.irr.obs
 	    a2 <- log_irr.obs
 	    alpha=1
