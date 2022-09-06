82704eb2d1c73f32c620b857ad48dc7d6a0307ff
Weinberger <daniel.weinberger@yale.edu>
2022-09-06 16:24:59 -0400

    update prior structure for commensurate gamma; prune number of models evaluated; update parameters to match LMIC setting for novavax for Hospitalized LRTI with RSv, rather than medically attended LRTI

--- R/jags_modified_pois_commensurate_gamma.R
+++ R/jags_modified_pois_commensurate_gamma.R
@@ -1,35 +1,27 @@
 model_string_commensurate_gamma <- "
 model{
 for(i in 1:2){ 
-
   N_cases_orig[i] ~ dpois(mu[i]) #Likelihood for data from original trial
  
    log(mu[i]) <- int_orig + (i-1)*delta + log(pop_orig[i]*100000)
-
   N_cases[i] ~ dpois(lambda[i]) #Likelihood for data from new trial
   
    log(lambda[i])  <- int  +  vax[i]*beta1 + log(pop[i]*100000)
   
-
 } 
   
 #Priors for original trial
+ # int_orig ~ dnorm(0, 0.1)
   int_orig ~ dnorm(0, 1e-4)
-
   delta ~ dnorm(0, 1e-4) #uninformative prior for original trial
-
 # Priors from new trial
-
-  int ~ dnorm(0, 1e-4)
-
+  int ~ dnorm(int_orig, tau2)
   beta1 ~ dnorm(delta, tau) #beta centered on alpha with highlight informative prior, which can become less informative if it does not match
-
  # tau ~ dgamma(1, 0.001) # gamma hyperprior from psborrow
   tau ~ dgamma(0.01, 0.01) # gamma hyperprior from psborrow
-
+  tau2 ~ dgamma(0.01, 0.01) # gamma hyperprior from psborrow
 	    a1 <- prec.log.irr.obs
 	    a2 <- log_irr.obs
 	    alpha=1
-
 }
 "
