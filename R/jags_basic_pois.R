b75f404f7b467df9ebf9e4a38dd7c5dfe65bd618
weinbergerlab <dweinber@gmail.com>
2022-12-07 17:08:48 -0500

    adds uniform priors, more comparisons

--- R/jags_basic_pois.R
+++ R/jags_basic_pois.R
@@ -5,9 +5,9 @@ for(i in 1:2){
 
   N_cases[i] ~ dpois(lambda[i])
   
-  log(lambda[i]) <- int  +  vax[i]*beta1 + log(pop[i]*100000)
+  log(lambda[i]) <- int  +  vax[i]*beta1 + log(pop[i]/100000)
   
-  log(mu[i]) <- int_orig + (i-1)*delta + log(pop_orig[i]*100000) #this is just included as a check--do we get same estimates out as provided in original study when we start with the counts?
+  log(mu[i]) <- int_orig + (i-1)*delta + log(pop_orig[i]/100000) #this is just included as a check--do we get same estimates out as provided in original study when we start with the counts?
 
 } 
   
@@ -15,12 +15,16 @@ for(i in 1:2){
 
   int ~ dnorm(0, 1e-4)
   
-  beta1 ~ dnorm(log_irr.obs, prec.log.irr.obs )
+  beta1 ~ dnorm(prior_mean_log_irr, prior_prec_log_irr )
 
 
   int_orig ~ dnorm(0, 1e-4)
 
   delta ~ dnorm(0, 1e-4)
+  
+  a1=set_tau_shp #does not do anything
+  a2=set_tau_rate #does not do anything
+  a3 = sd.upper
 
   tau=1
   alpha=1
